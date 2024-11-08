4ec42ad20d4eac6a5bef2d4896db821ab123a64a
Ehsan-1998 <Faizeeehsan@gmail.com>
2024-11-08 12:54:38 -0500

    Successfully created the views for the enclosure-view and composite-animal-collection-view

---
+++ src/main/java/com/example/ZooApplication/Controllers/EnclosureViewController.java
@@ -0,0 +1,26 @@
+package com.example.ZooApplication.Controllers;
+
+import com.example.ZooApplication.Model.Animal;
+import com.example.ZooApplication.Model.Enclosure;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class EnclosureViewController {
+
+
+
+    @FXML
+    private ListView<String> animalListView;
+
+    public void setEnclosure(Enclosure enclosure) {
+        ObservableList<String> animalNames = FXCollections.observableArrayList();
+        for (Animal animal : enclosure.getAnimals()) {
+            animalNames.add(animal.getName());
+        }
+        animalListView.setItems(animalNames);
+    }
+
+
+}
